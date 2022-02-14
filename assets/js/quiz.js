f6ab1d8f85674748317fc6d16a378ef22e1fc9f0
awenelo <56133692+awenelo@users.noreply.github.com>
2022-02-14 17:23:51 -0500

    Add some styling to the quiz

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -6,14 +6,15 @@ function next_quiz_question(question_div, answers_div, quiz_data, score, correct
     answers_div.innerHTML = "";
 
     for (let question_index = 0; question_index < quiz_data[0].answers.length; question_index++) {
-        let question_button = document.createElement("button");
-        question_button.innerText = quiz_data[0].answers[question_index];
+        let answer_button = document.createElement("button");
+        answer_button.innerText = quiz_data[0].answers[question_index];
+        answer_button.classList.add("quiz_answer_button");
         if (quiz_data.length > 1) {
-            question_button.onclick = () => {next_quiz_question(question_div, answers_div, quiz_data.slice(1), score, quiz_data[0].correct_answer, question_index);};
+            answer_button.onclick = () => {next_quiz_question(question_div, answers_div, quiz_data.slice(1), score, quiz_data[0].correct_answer, question_index);};
         } else {
-            question_button.onclick = () => {display_quiz_results(question_div, answers_div, score, quiz_data[0].correct_answer, question_index);};
+            answer_button.onclick = () => {display_quiz_results(question_div, answers_div, score, quiz_data[0].correct_answer, question_index);};
         }
-        answers_div.appendChild(question_button);
+        answers_div.appendChild(answer_button);
     }
 }
 
