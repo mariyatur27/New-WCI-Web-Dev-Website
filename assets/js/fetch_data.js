fcfb8c80f57b45a81f2598315d71c01fa9f06e84
mariyatur27 <63456223+mariyatur27@users.noreply.github.com>
2022-03-07 16:04:32 -0500

    Adding elements to the portfolio

--- assets/js/fetch_data.js
+++ assets/js/fetch_data.js
@@ -6,6 +6,7 @@ var members = [
         "instagram": "",
         "linkedin": "",
         "github": "",
+        "photo": "https://lh3.googleusercontent.com/EbXw8rOdYxOGdXEFjgNP8lh-YAuUxwhOAe2jhrz3sgqvPeMac6a6tHvT35V6YMbyNvkZL4R_a2hcYBrtfUhLvhf-N2X3OB9cvH4uMw=w1064-v0",
         "projects": ["WCI Extracurriculars Website", "New WCI Web-Dev Website"]
     }
 ];
--- assets/js/team.js
+++ assets/js/team.js
@@ -9,6 +9,19 @@ async function buildTeamMember(id=null) {
     for (const member of members) {
         if (id == null || member.id == id) {
             console.log(member);
+            let box = document.createElement('div'); box.classList.add('a_box');
+                let photo = document.createElement('img'); photo.src = member.photo; photo.style.width = '90%';
+                box.appendChild(photo);
+                let info = document.createElement('div'); info.classList.add('info');
+                    let name = document.createElement('h3'); name.innerText = member.name;
+                    info.appendChild(name);
+                    let grade = document.createElement('h6'); grade.innerText = "Grade: ".concat(member.grade);
+                    info.appendChild(grade);
+                    let bio = document.createElement('p'); bio.innerText = member.bio;
+                    info.appendChild(bio);
+                let socials = document.createElement('div'); socials.classList.add('socials_links');
+                box.appendChild(info);
+            section.appendChild(box);
         }
     }
 }
