33a2a90130b4a5920d20d7b4de676d2c1cdaa9e5
awenelo <56133692+awenelo@users.noreply.github.com>
2022-03-21 15:46:03 -0400

    Display team members in a grid

--- assets/js/fetch_data.js
+++ assets/js/fetch_data.js
@@ -8,7 +8,57 @@ var members = [
         "github": "",
         "photo": "https://lh3.googleusercontent.com/EbXw8rOdYxOGdXEFjgNP8lh-YAuUxwhOAe2jhrz3sgqvPeMac6a6tHvT35V6YMbyNvkZL4R_a2hcYBrtfUhLvhf-N2X3OB9cvH4uMw=w1064-v0",
         "projects": ["WCI Extracurriculars Website", "New WCI Web-Dev Website"]
-    }
+    },
+    {
+        "name": "Nuha",
+        "grade": "11",
+        "bio": "I am Nuha",
+        "instagram": "",
+        "linkedin": "",
+        "github": "",
+        "photo": "https://lh3.googleusercontent.com/EbXw8rOdYxOGdXEFjgNP8lh-YAuUxwhOAe2jhrz3sgqvPeMac6a6tHvT35V6YMbyNvkZL4R_a2hcYBrtfUhLvhf-N2X3OB9cvH4uMw=w1064-v0",
+        "projects": ["WCI Extracurriculars Website", "New WCI Web-Dev Website"]
+    },
+    {
+        "name": "Nuha",
+        "grade": "11",
+        "bio": "I am Nuha",
+        "instagram": "",
+        "linkedin": "",
+        "github": "",
+        "photo": "https://lh3.googleusercontent.com/EbXw8rOdYxOGdXEFjgNP8lh-YAuUxwhOAe2jhrz3sgqvPeMac6a6tHvT35V6YMbyNvkZL4R_a2hcYBrtfUhLvhf-N2X3OB9cvH4uMw=w1064-v0",
+        "projects": ["WCI Extracurriculars Website", "New WCI Web-Dev Website"]
+    },
+    {
+        "name": "Nuha",
+        "grade": "11",
+        "bio": "I am Nuha",
+        "instagram": "",
+        "linkedin": "",
+        "github": "",
+        "photo": "https://lh3.googleusercontent.com/EbXw8rOdYxOGdXEFjgNP8lh-YAuUxwhOAe2jhrz3sgqvPeMac6a6tHvT35V6YMbyNvkZL4R_a2hcYBrtfUhLvhf-N2X3OB9cvH4uMw=w1064-v0",
+        "projects": ["WCI Extracurriculars Website", "New WCI Web-Dev Website"]
+    },
+    {
+        "name": "Nuha",
+        "grade": "11",
+        "bio": "I am Nuha",
+        "instagram": "",
+        "linkedin": "",
+        "github": "",
+        "photo": "https://lh3.googleusercontent.com/EbXw8rOdYxOGdXEFjgNP8lh-YAuUxwhOAe2jhrz3sgqvPeMac6a6tHvT35V6YMbyNvkZL4R_a2hcYBrtfUhLvhf-N2X3OB9cvH4uMw=w1064-v0",
+        "projects": ["WCI Extracurriculars Website", "New WCI Web-Dev Website"]
+    },
+    {
+        "name": "Nuha",
+        "grade": "11",
+        "bio": "I am Nuha",
+        "instagram": "",
+        "linkedin": "",
+        "github": "",
+        "photo": "https://lh3.googleusercontent.com/EbXw8rOdYxOGdXEFjgNP8lh-YAuUxwhOAe2jhrz3sgqvPeMac6a6tHvT35V6YMbyNvkZL4R_a2hcYBrtfUhLvhf-N2X3OB9cvH4uMw=w1064-v0",
+        "projects": ["WCI Extracurriculars Website", "New WCI Web-Dev Website"]
+    },
 ];
 
 var dataFetched = false;
